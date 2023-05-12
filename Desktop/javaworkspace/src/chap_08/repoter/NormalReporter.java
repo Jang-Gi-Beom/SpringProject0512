dd95eeab7e2bac4ec592cb18a6556330da2fa13c
Jang-Gi-Beom <118677505+Jang-Gi-Beom@users.noreply.github.com>
2023-05-12 15:58:47 +0900

    Delete Desktop/javaworkspace directory

--- Desktop/javaworkspace/src/chap_08/repoter/NormalReporter.java
+++
@@ -1,8 +0,0 @@
-package chap_08.repoter;
-
-public class NormalReporter implements Reportable{
-    @Override //뼈대만 있는 것을 구현함
-    public void report() {
-        System.out.println("일반 화재 신고 진행함");
-    }
-}
--- Desktop/javaworkspace/src/chap_08/repoter/Reportable.java
+++
@@ -1,6 +0,0 @@
-package chap_08.repoter;
-
-public interface Reportable {
-    void report(); //신고  (뼈대만 있음)
-
-}
